2faf682b3eee950fca3be1efe41dfc674e836998
RubenPereira <rpbeat@gmail.com>
2016-09-28 15:44:16 +0100

    Insert files

---
+++ app/src/main/java/com/example/rpbeat/gitaplication/GitAplication.java
@@ -0,0 +1,13 @@
+package com.example.rpbeat.gitaplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GitAplication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_aplication);
+    }
+}
